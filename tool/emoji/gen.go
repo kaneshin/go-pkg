6029a77cacbfd1951c2bf58be9023184ea05892e
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-09-10 12:56:10 +0900

    Modify emoji

---
+++ tool/emoji/gen.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+	"text/template"
+)
+
+// main ...
+func main() {
+	dat, err := ioutil.ReadFile("./emoji.dat")
+	if err != nil {
+		panic(err)
+	}
+	r16 := []string{}
+	r32 := []string{}
+	for _, r := range string(dat) {
+		if 0xfff0000&int(r) == 0 {
+			r16 = append(r16, fmt.Sprintf("0x%04x", r))
+		} else {
+			r32 = append(r32, fmt.Sprintf("0x%04x", r))
+		}
+	}
+	t := template.Must(template.New("").Parse(text))
+	data := map[string]interface{}{
+		"R16": r16,
+		"R32": r32,
+	}
+	var writer bytes.Buffer
+	if err := t.Execute(&writer, data); err != nil {
+		panic(err)
+	}
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	f, err := os.Create(path.Join(pwd, "emoji.go"))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	_, err = f.Write(writer.Bytes())
+	if err != nil {
+		panic(err)
+	}
+	f.Sync()
+}
+
+const text = `package unicode
+
+import (
+	"unicode"
+)
+
+var e = &unicode.RangeTable{
+	R16: []unicode.Range16{{printf "{"}}{{range .R16}}{{printf "\n\t\t{%s, %s, 1}," . .}}{{end}}
+	},
+	R32: []unicode.Range32{{printf "{"}}{{range .R32}}{{printf "\n\t\t{%s, %s, 1}," . .}}{{end}}
+	},
+	LatinOffset: 2,
+}
+
+// IsEmoji reports whether the rune is a emoji.
+func IsEmoji(r rune) bool {
+	return unicode.In(r, e)
+}`
