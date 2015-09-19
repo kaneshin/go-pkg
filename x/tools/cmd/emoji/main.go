95131f941a5e893ea615d5dfb7e1d964b4106675
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-09-19 14:40:13 +0900

    Modify emoji generator

--- x/tools/cmd/emoji/main.go
+++ x/tools/cmd/emoji/main.go
@@ -6,28 +6,57 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"sort"
 	"text/template"
+)
+
+const (
+	in    = "emoji.dat"
+	out   = "emoji.go"
+	ascii = 0x7f
 )
 
 // main ...
 func main() {
-	dat, err := ioutil.ReadFile("./emoji.dat")
+	dat, err := ioutil.ReadFile(in)
 	if err != nil {
 		panic(err)
 	}
-	r16 := []string{}
-	r32 := []string{}
-	offset := 0
+	m16 := map[int]int{}
+	m32 := map[int]int{}
 	for _, r := range string(dat) {
-		if 0xfff0000&int(r) == 0 {
-			r16 = append(r16, fmt.Sprintf("0x%04x", r))
-		} else {
-			r32 = append(r32, fmt.Sprintf("0x%04x", r))
+		n := int(r)
+		if n <= ascii {
+			continue
 		}
-		if int(r) <= 0xff {
-			offset++
+		if 0xffff0000&n == 0 {
+			m16[n] = n
+		} else {
+			m32[n] = n
 		}
 	}
+	m2s := func(m map[int]int, u ...int) (s []string, o int) {
+		i := []int{}
+		for _, v := range m {
+			i = append(i, v)
+		}
+		sort.Sort(sort.IntSlice(i))
+		t := func() int {
+			if len(u) > 0 {
+				return u[0]
+			}
+			return 0x0
+		}()
+		for _, v := range i {
+			s = append(s, fmt.Sprintf("0x%04x", v))
+			if v <= t {
+				o++
+			}
+		}
+		return
+	}
+	r16, offset := m2s(m16, 0xff)
+	r32, _ := m2s(m32)
 	t := template.Must(template.New("").Parse(text))
 	data := map[string]interface{}{
 		"R16":         r16,
@@ -42,7 +71,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	f, err := os.Create(path.Join(pwd, "emoji.go"))
+	f, err := os.Create(path.Join(pwd, out))
 	if err != nil {
 		panic(err)
 	}
@@ -71,4 +100,5 @@ var e = &unicode.RangeTable{
 // IsEmoji reports whether the rune is a emoji.
 func IsEmoji(r rune) bool {
 	return unicode.In(r, e)
-}`
+}
+`
